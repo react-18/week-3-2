13424e28a80b449116f16b74a1c4325bcd4b7b6d
gy <gayunlee11@gmail.com>
2022-02-17 22:15:25 +0900

    Chore: Redux 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,20 @@ import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../styles/globalStyle';
 import { theme } from '../styles/theme';
+import { store } from '../store/store';
+import { Provider } from 'react-redux';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Head>
-        <title>React 18</title>
-      </Head>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Head>
+          <title>React 18</title>
+        </Head>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 };
 
